a2e779bb421b95fbc32e30c3e0b1569359dc127e
Tomas Zeman <tomas.zeman@showmax.com>
2022-05-06 11:40:37 +0200

    Proper URL on share

--- src/components/widgets/Share.svelte
+++ src/components/widgets/Share.svelte
@@ -13,7 +13,7 @@
 	}/${state.board.words.length}\n\n    ${state.board.state
 		.slice(0, state.guesses)
 		.map((r) => r.join(""))
-		.join("\n    ")}\nmikhad.github.io/wordle`;
+		.join("\n    ")}\nhttps://tomaas-zeman.github.io/wordle-cz/#daily`;
 </script>
 
 <div
